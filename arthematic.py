5d9d9d5616a9041ccdccdcd9e1e918b840626707
Abhinav Reddy <abhinavreddy208@gmail.com>
2018-01-31 03:44:22 +0530

    initial

---
+++ arthematic.py
@@ -0,0 +1,22 @@
+# addition
+a=5
+b=6
+bb=6.0
+
+c=a+b
+
+print (c)
+# subtraction
+d = b-a
+
+print(d)
+# multiplication
+e = a*b
+
+print(e)
+
+# division
+f = bb/a
+
+print(f)
+
---
+++ loop.py
@@ -0,0 +1,20 @@
+# For Loop
+
+sum = 0
+for i  in range(5):
+    sum = sum + i
+    print (sum)
+
+
+# while loop
+
+
+while True:
+      print("Enter the Number of bats:  ")
+      bats = float(input())
+      print("Enter the number of balls:  ")
+      balls = float(input())
+      if balls == 0:
+         continue
+      print (bats + balls)
+
